a1ac6baa7b14f79c2b199c7c9c9c2e2aa693c215
piyoos kshyap <piyooskshyap@gmail.com>
2024-12-13 23:13:55 +0530

    2

--- src/App.jsx
+++ src/App.jsx
@@ -8,8 +8,7 @@ import Page6 from './page/Page6'
 import Page7 from './page/Page7'
 import Page8 from './page/Page8'
 import Page9 from './page/Page9'
-import Page10 from './page/Page10'
-// import Page11 from './page/Page11'
+import Page10 from './page/Page10' 
 import Header from './components/Header'
 const App = () => {
   return (
